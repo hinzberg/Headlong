7d1d0b3ea3c50d2df53be4a2a1e24569dad56938
Holger Hinzberg <holger@hinzberg.de>
2021-10-14 16:32:47 +0200

    Initial Commit

---
+++ Headlong/HeadlongApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadlongApp.swift
+//  Headlong
+//
+//  Created by Holger Hinzberg on 14.10.21.
+//
+
+import SwiftUI
+
+@main
+struct HeadlongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
