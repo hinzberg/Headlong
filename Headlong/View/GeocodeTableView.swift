6242b1a08e337d813d6c63838417d75dc47faa03
Holger Hinzberg <holger@hinzberg.de>
2021-11-03 17:43:12 +0100

    Searchable added

--- Headlong/View/GeocodeTableView.swift
+++ Headlong/View/GeocodeTableView.swift
@@ -7,6 +7,22 @@ import SwiftUI
 struct GeocodeTableView: View {
     
     @EnvironmentObject  var geocodeRepository : GeocodeLocationRepository
+    @State private var searchText = ""
+    
+    init() {
+        let customAppearance = UINavigationBarAppearance()
+        // Backgroundcolor
+        customAppearance.backgroundColor = UIColor(red: 0.93, green: 0.93, blue: 1.0, alpha: 1)
+        // Font color for navigationBarTitleDisplayMode large
+        customAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor(Color.cocoaBlue)]
+        // Font color for navigationBarTitleDisplayMode inline
+        customAppearance.titleTextAttributes = [.foregroundColor: UIColor(Color.cocoaBlue)]
+        
+        UINavigationBar.appearance().standardAppearance = customAppearance
+        UINavigationBar.appearance().compactAppearance = customAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = customAppearance
+    }
+    
     
     var body: some View {
         
@@ -30,6 +46,14 @@ struct GeocodeTableView: View {
                         
                     }.listRowSeparatorTint( Color.cocoaBlue)
                 }.listStyle(.plain)
+                
+                    .searchable(
+                        text: $searchText,
+                        placement: .navigationBarDrawer(displayMode: .always),
+                        prompt: "Search ...")
+                    .onChange(of: searchText) { searchText in
+                        print(searchText)
+                    }
                 
                     .navigationBarTitle("Headlong", displayMode: .inline)
                     .navigationTitle("Back")
